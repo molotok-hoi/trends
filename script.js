888440dd85e7428f4ff0828d410c3543fa39e62a
molotok-hoi <80571475+molotok-hoi@users.noreply.github.com>
2021-03-13 11:45:04 +0300

    new trends

---
+++ script.js
@@ -0,0 +1,12 @@
+$(function() {
+  $("[data-scroll]").on("click", function(event) {
+        event.preventDefault();
+
+        let elementId = $(this).data('scroll');
+        let elementOffset = $(elementId).offset().top;
+
+        $("html, body").animate({
+            scrollTop: elementOffset 
+        }, 700);
+    });
+});    
